456889f84bd6dbc795f0d835b83857c236e1add1
jj96fr <freiff88@gmail.com>
2023-02-21 17:08:44 +0100

    starting

---
+++ assets/script/resultpage.js
@@ -0,0 +1,150 @@
+let param = new URLSearchParams(location.search);
+let correct_answers = param.get("correct_answers");
+let questions = param.get("tot_questions");
+
+let outPercPos = 0; // percentuale risposte positive
+let outPercNeg = 0; // percentuale risposte negative
+let didYouPass = Boolean; // esito test, + di 60% è passato
+
+// riceve risultato test e numero totale domande
+//produce percentuali giuste e sbagliate ed esito del test
+const resultsTest = (cor, tot) => {
+  outPercPos = Math.round((cor / tot) * 100); // calcola percentuale risposte positive
+  outPercNeg = Math.round((1 - cor / tot) * 100);
+  console.log(" percentuali corrette/sbagliate", outPercPos, outPercNeg);
+
+  const posPercentage = document.querySelector(".percentPos"); // selettore nodo percentuale positiva
+  posPercentage.innerText = `${outPercPos}`;
+
+  const negPercentage = document.querySelector(".percentNeg"); // calcola percentuale risposte negative
+  negPercentage.innerText = `${outPercNeg}`; // selettore nodo percentuale negativa
+
+  const posQuestion = document.getElementById("posQuestion");
+  posQuestion.innerText = `${cor}/${tot}`;
+
+  const negQuestion = document.querySelector("#negQuestion");
+  negQuestion.innerText = `${tot - cor}/${tot}`;
+
+  if (Math.round(outPercPos) >= 60) {
+    // test passaggio esame + 60%
+    // aggiunta immagini
+    didYouPass = true;
+    document.querySelector(".win").src = "./assets/audio/RockyTheme.mp3";
+    const easterEggsSel = document.querySelectorAll(".easterEggsWin");
+    easterEggsSel.forEach(element =>{
+      element.className = ".passedTest";
+      element.style.width = "10%";
+      element.style.position = "absolute";
+      element.style.transform = "translate(-50%, -50%)";
+    })
+    const easterEggsImgSel = document.querySelectorAll(".img");
+    easterEggsImgSel.forEach(element =>{
+      element.style.width = "100%";
+      element.style.height = "18vh";
+    });
+
+    const passedTestSel = document.querySelectorAll(".passedTest");
+    passedTestSel.forEach(element => {
+      element.style.visibility = "visible";
+    });
+
+    const innerCirclePhoto = document.querySelector(".innerCircle");
+    innerCirclePhoto.style.backgroundImage = "url(./assets/img/grosso.jpeg)";
+    innerCirclePhoto.style.backgroundSize = "cover";
+    innerCirclePhoto.style.borderRadius = "50%";
+
+  } else if (Math.round(outPercPos) < 60) {
+    didYouPass = false;
+    document.querySelector(".lose").src = "./assets/audio/bara.mp3";
+    document.querySelector(".lose").volume = 0.5;
+
+    const easterEggsSel = document.querySelectorAll(".easterEggsLose");
+    easterEggsSel.forEach(element =>{
+      element.className = ".notPassedTest";
+      element.style.width = "10%";
+      element.style.position = "absolute";
+      element.style.transform = "translate(-50%, -50%)";
+    })
+    const easterEggsImgSel = document.querySelectorAll(".img");
+    easterEggsImgSel.forEach(element =>{
+      element.style.width = "120%";
+      element.style.height = "20vh";
+    });
+
+    const passedTestSel = document.querySelectorAll(".notPassedTest");
+    passedTestSel.forEach(element => {
+      element.style.visibility = "visible";
+    });
+
+    const innerCirclePhoto = document.querySelector(".innerCircle");
+    innerCirclePhoto.style.backgroundImage = "url(./assets/img/fail1.jpeg)";
+    innerCirclePhoto.style.backgroundSize = "cover";
+    innerCirclePhoto.style.borderRadius = "50%";
+    innerCirclePhoto.style.color = "red";
+    innerCirclePhoto.style.fontWeight = "bold";
+    innerCirclePhoto.style.fontSize = "1em";
+
+
+    const failedTest = document.querySelector(".innerCircle");
+    failedTest.innerHTML = `<h4 class="congSubtitle">
+              failed test! <br />
+              <span class="blue">you did't pass the exam.</span>
+            </h4>
+            <p>We'll send you the report in few minutes. Check your email (including promotion / spam folder)</p>`;
+    const blueAdvise = document.querySelector("div.innerCircle span.blue");
+    blueAdvise.style.color = "darkViolet";
+  } else {
+    return 0;
+  }
+  console.log(outPercPos, outPercNeg, didYouPass);
+  circlePercentage(outPercNeg);
+  return 1;
+};
+
+// console.log(outPercPos, outPercNeg, didYouPass);
+
+const circlePercentage = (redCircleAmount) => {
+  // converto percentuali in gradi di rotazione
+  console.log("circlePercentage eseguita, input della funzione", redCircleAmount);
+  let degConv = Math.round(1 - redCircleAmount * 3.6);
+
+  // creo i selettori per i cerchi
+  const circleAcqua = document.querySelector(".color1");
+  const circlePink = document.querySelector(".color2");
+  const circleBackground = document.querySelector(".color3");
+
+  // centro e ruoto i cerchi per evitare conflitti con foglio css
+  circlePink.style.transform = `translate(-50%,-50%) rotate(${1 - degConv - 46}deg)`;
+  circlePink.style.top = "50%";
+  circlePink.style.left = "50%";
+  circleAcqua.style.transform = `translate(-50%,-50%) rotate(${-46}deg)`;
+
+  //stabilisco orientamento in base alla percentuale delle risposte giuste
+  // lavorando di spicchi ogni 25% bisogna cambiare anche la colorazione e non solo la gradazione
+  if (redCircleAmount <= 25) {
+  } else if (redCircleAmount >= 25 && redCircleAmount < 50) {
+    circlePink.style.borderColor = "#d20094 transparent transparent #d20094";
+    circleAcqua.style.borderColor = "#00ffff transparent transparent #00ffff";
+  } else if (redCircleAmount >= 50 && redCircleAmount < 75) {
+    circlePink.style.borderColor = "#d20094 #00ffff transparent #d20094";
+    circleAcqua.style.borderColor = "#00ffff transparent transparent transparent";
+    circleBackground.style.borderColor = "#d20094 #d20094 #d20094 #00ffff";
+  } else if (redCircleAmount >= 75 && redCircleAmount < 100) {
+    circlePink.style.borderColor = "#d20094 transparent #d20094 #d20094";
+    circleAcqua.style.borderColor = "transparent #d20094 transparent transparent";
+    circleBackground.style.borderColor = "#00ffff";
+  } else if ((redCircleAmount = 100)) {
+    circlePink.style.borderColor = "#d20094";
+    circleAcqua.style.borderColor = "transparent";
+    circleBackground.style.borderColor = "#d20094";
+  } else {
+    return 0;
+  }
+  return 1;
+};
+
+//resultsTestConverter(correct_answers, questions);
+//circlePercentage(outPercNeg);
+resultsTest(correct_answers, questions);
+
+//variabili più leggibili (non fare combinazioni invertite)
